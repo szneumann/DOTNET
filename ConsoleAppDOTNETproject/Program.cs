3072fabc8b7a7cbe3b4c9859ce99531340515b44
Szymon Neumann <szymon22337@gmail.com>
2024-04-23 17:51:58 +0200

    zad 1, 2, start3

---
+++ ConsoleAppDOTNETproject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Samochod s1 = new Samochod("Fiat", "126p", 2, 650, 6.0);
+        Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
+        Garaz g1 = new Garaz();
+        g1.Adres = "ul. Garażowa 1";
+        g1.Pojemnosc = 1;
+        Garaz g2 = new Garaz("ul. Garażowa 2", 2);
+        g1.WprowadzSamochod(s1);
+        g1.WypiszInfo();
+        g1.WprowadzSamochod(s2);
+        g2.WprowadzSamochod(s2);
+        g2.WprowadzSamochod(s1);
+        g2.WypiszInfo();
+
+        g2.WyprowadzSamochod();
+        g2.WypiszInfo();
+
+        g2.WyprowadzSamochod();
+        g2.WyprowadzSamochod();
+        Console.ReadKey();
+
+    }
+}
+
+
---
+++ Zadanie3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        string path = @"C:\ścieżka\do\pliku.txt";
+        try
+        {
+            // Otwarcie strumienia do zapisu do pliku (jeśli nie istnieje, zostanie utworzony)
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                // Tworzenie ciągu znaków do zapisania
+                string data = "To jest przykładowy tekst do zapisania w pliku.";
+                // Konwersja ciągu znaków na tablicę bajtów w formacie UTF-8
+                byte[] byteData = Encoding.UTF8.GetBytes(data);
+                // Zapis danych do pliku
+                fs.Write(byteData, 0, byteData.Length);
+                Console.WriteLine("Dane zostały zapisane do pliku.");
+            }
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Wystąpił błąd wejścia/wyjścia: {ex.Message}");
+        }
+    }
+}
