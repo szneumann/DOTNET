1f044671475df37123ded71f9a1f3ee3bf344867
Szymon Neumann <szymon22337@gmail.com>
2024-04-25 19:20:50 +0200

    zad6

---
+++ Zadanie6/Zadanie6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            // Ścieżka do pliku źródłowego
+            string sourceFilePath = @"C:\Users\dev\Desktop\tekst.txt";
+
+            // Ścieżka do pliku docelowego
+            string destinationFilePath = @"C:\Users\dev\Desktop\nowytekst.txt";
+
+            // Kopiowanie pliku
+            File.Copy(sourceFilePath, destinationFilePath, true);
+
+            Console.WriteLine("Plik został skopiowany pomyślnie.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Wystąpił błąd podczas kopiowania pliku: " + ex.Message);
+        }
+        Console.ReadKey();
+    }
+}
