fdb3f98f00eaccec57d296d84231108f10dbe5c7
Szymon Neumann <szymon22337@gmail.com>
2024-05-04 21:37:30 +0200

    2_3

--- Zadanie2_3/Zadanie2_3/Form1.Designer.cs
+++ Zadanie2_3/Zadanie2_3/Form1.Designer.cs
@@ -30,33 +30,66 @@
         {
             this.lblCPU = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.maxiram = new System.Windows.Forms.TextBox();
+            this.thread_lbl = new System.Windows.Forms.TextBox();
+            this.threadtotalbox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblCPU
             // 
-            this.lblCPU.Location = new System.Drawing.Point(101, 118);
+            this.lblCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPU.Location = new System.Drawing.Point(101, 54);
             this.lblCPU.Name = "lblCPU";
-            this.lblCPU.Size = new System.Drawing.Size(100, 20);
+            this.lblCPU.ReadOnly = true;
+            this.lblCPU.Size = new System.Drawing.Size(313, 31);
             this.lblCPU.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(253, 199);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(101, 268);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(468, 31);
             this.textBox2.TabIndex = 3;
+            // 
+            // maxiram
+            // 
+            this.maxiram.Location = new System.Drawing.Point(101, 321);
+            this.maxiram.Name = "maxiram";
+            this.maxiram.Size = new System.Drawing.Size(474, 20);
+            this.maxiram.TabIndex = 4;
+            // 
+            // thread_lbl
+            // 
+            this.thread_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thread_lbl.Location = new System.Drawing.Point(101, 130);
+            this.thread_lbl.Name = "thread_lbl";
+            this.thread_lbl.Size = new System.Drawing.Size(606, 31);
+            this.thread_lbl.TabIndex = 5;
+            // 
+            // threadtotalbox
+            // 
+            this.threadtotalbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.threadtotalbox.Location = new System.Drawing.Point(101, 194);
+            this.threadtotalbox.Name = "threadtotalbox";
+            this.threadtotalbox.Size = new System.Drawing.Size(606, 31);
+            this.threadtotalbox.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.threadtotalbox);
+            this.Controls.Add(this.thread_lbl);
+            this.Controls.Add(this.maxiram);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.lblCPU);
             this.Cursor = System.Windows.Forms.Cursors.AppStarting;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +99,9 @@
 
         private System.Windows.Forms.TextBox lblCPU;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox maxiram;
+        private System.Windows.Forms.TextBox thread_lbl;
+        private System.Windows.Forms.TextBox threadtotalbox;
     }
 }
 
--- Zadanie2_3/Zadanie2_3/Form1.cs
+++ Zadanie2_3/Zadanie2_3/Form1.cs
@@ -17,31 +17,63 @@ namespace Zadanie2_3
     {
         private PerformanceCounter cpuCounter;
         private PerformanceCounter ramCounter;
+        private PerformanceCounter maxRAM;
+        private PerformanceCounter threadCounter;
+        private PerformanceCounter threadCounter_total;
         public Form1()
         {
             InitializeComponent();
             InitialiseCPUCounter();
+            InitialiseRAMCounter();
         }
         // Inicjalizacja liczników
         private void InitialiseCPUCounter()
         {
             cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-            Console.WriteLine("Inicjalizacja cpuCounter");
-            Console.WriteLine(cpuCounter.NextValue());
+            // Dla bieżącego procesu:
+            threadCounter = new PerformanceCounter("Process", "Thread Count", Process.GetCurrentProcess().ProcessName);
+            // Dla całego systemu
+            threadCounter_total = new PerformanceCounter("Process", "Thread Count", "_Total");
+        }
+        private void InitialiseRAMCounter()
+        {
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            maxRAM = new PerformanceCounter("Hyper-v Dynamic Memory VM", "Physical Memory");
         }
 
-                  
-
+        // Załadowanie strony głównej
         private void Form1_Load(object sender, EventArgs e)
         {
-            float tmp = cpuCounter.NextValue();
-            lblCPU.Text = String.Format("{0} %", tmp);
-            Console.WriteLine("Form1_Load");
+            maxiram.Text = maxRAM.ToString();
+            // Wywołaj metodę aktualizacji liczników co sekundę
+            Timer timer = new Timer();
+            timer.Interval = 1000; // 1000 milisekund = 1 sekunda
+            timer.Tick += new EventHandler((s, ev) => UpdateCounters());
+            timer.Start();
         }
+        // updater wartości cpu i RAM    
+       private void UpdateCounters()
+        {
+            // Pobierz wartości z liczników
+            float cpuUsage = cpuCounter.NextValue();
+            float availableRAM = ramCounter.NextValue();
+            float threadCount = threadCounter.NextValue();
+            float threadCounttotal = threadCounter_total.NextValue();
 
+            // Wyświetl wartości w kontrolkach
+            lblCPU.Text = $"Użycie procesora: {cpuUsage}%";
+            textBox2.Text = $"Dostępna pamięć RAM: {availableRAM} MB";
+            thread_lbl.Text = $"Wartość licznika liczby wątków: {threadCount}";
+            threadtotalbox.Text = $"Wartość dla całego systemu: {threadCounttotal}";
+        }
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
